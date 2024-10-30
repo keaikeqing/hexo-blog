5cfc9c56852cf162b6425fa316e6679cf8502522
keaikeqing <18822187196@163.com>
2024-10-30 19:45:53 +0800

    正式上线

--- scripts/image-filter.js
+++ scripts/image-filter.js
@@ -10,19 +10,20 @@ hexo.extend.filter.register("after_render:html", function (htmlContent) {
         width = sizeMatch[1];
         height = sizeMatch[2];
         src = src.replace(/#(\d+)x(\d+)/, "");
-      }
 
-      return `
-          <div style="text-align: center;">
-              <img src="${src}" alt="${alt}" ${rest} 
-                  ${
-                    width && height
-                      ? `style="display: block; margin: 0 auto; width: ${width}%; height: ${height}%; max-width: 500px; max-height: 500px;"`
-                      : 'style="display: block; margin: 0 auto; max-width: 500px; max-height: 500px;"'
-                  }>
-              <span style="margin-top: 10px; text-decoration: underline; text-underline-offset: 2px; text-decoration-color: #d9d9d9; font-size: 20px; display: block;">${title}</span>
-          </div>
-      `;
+        return `
+            <div style="text-align: center;">
+                <img src="${src}" alt="${alt}" ${rest} 
+                    ${
+                      width && height
+                        ? `style="display: block; margin: 0 auto; width: ${width}%; height: ${height}%; max-width: 500px; max-height: 500px;"`
+                        : 'style="display: block; margin: 0 auto; max-width: 500px; max-height: 500px;"'
+                    }>
+                <span style="margin-top: 10px; text-decoration: underline; text-underline-offset: 2px; text-decoration-color: #d9d9d9; font-size: 20px; display: block;">${title}</span>
+            </div>
+        `;
+      }
+      return match;
     }
   );
 });
